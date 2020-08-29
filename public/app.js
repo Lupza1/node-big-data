e029c9ecebe58c072cc8eb8ab694918c47478adb
Lupza1 <59704851+Lupza1@users.noreply.github.com>
2020-08-29 14:11:22 -0300

    second commit

--- public/app.js
+++ public/app.js
@@ -1,43 +1,4 @@
 //Inicializador del elemento Slider
-
-document.querySelector('#buscar').addEventListener('click', traerDatos)
-
-function traerDatos(){
-
-  const xhttp = new XMLHttpRequest();
-
-  xhttp.open('GET', 'data.json', true)
-
-  xhttp.send()
-
-  xhttp.onreadystatechange = function(){
-
-    if(this.readyState == 4 && this.status == 200){
-
-      let datos = JSON.parse(this.responseText);
-
-      let buscar = document.querySelector('#lista');
-
-      buscar.innerHTML = ''
-
-      for(let item of datos) {
-
-        buscar.innerHTML +=`
-          <tr>
-            <td>${item.Id}</td>
-            <td>${item.Direccion}</td>
-            <td>${item.Ciudad}</td>
-            <td>${item.Telefono}</td>
-            <td>${item.Codigo_Postal}</td>
-            <td>${item.Tipo}</td>
-            <td>${item.Precio}</td>
-          </tr>
-        `
-      }
-    }
-  }
-}
-
 $("#rangoPrecio").ionRangeSlider({
   type: "double",
   grid: false,
@@ -64,3 +25,64 @@ function setSearch() {
 $('select').material_select();
 
 setSearch()
+
+document.querySelector('#buscar').addEventListener('click', traerDatos)
+
+function traerDatos(){
+
+  var ciud = document.getElementById("ciudad").value
+
+  var tipo = document.getElementById("tipo").value
+
+  const xhttp = new XMLHttpRequest();
+
+  xhttp.open('GET', 'data.json', true)
+
+  xhttp.send()
+
+  xhttp.onreadystatechange = function(){
+
+    if(this.readyState == 4 && this.status == 200){
+
+      let datos = JSON.parse(this.responseText);
+
+      datos.filter(item =>{
+        if (ciud == "") {
+          return item.Ciudad
+        } else {
+          return item.Ciudad = ciud
+        }
+
+      })
+
+      datos.filter(item =>{
+        if (tipo == "") {
+          return item.Tipo
+        } else {
+          return item.Tipo = tipo
+        }
+      })
+
+      let buscar = document.querySelector('#lista');
+
+      buscar.innerHTML = ''
+
+      for(let item of datos) {
+
+        buscar.innerHTML +=`
+          <tr>
+            <p>
+              <td>${item.Id}</td>
+              <td>${item.Direccion}</td>
+              <td>${item.Ciudad}</td>
+              <td>${item.Telefono}</td>
+              <td>${item.Codigo_Postal}</td>
+              <td>${item.Tipo}</td>
+              <td>${item.Precio}</td>
+            <hr>
+          </tr>
+        `
+      }
+    }
+  }
+}
